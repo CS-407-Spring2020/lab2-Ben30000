fdb187a0b70bc8283818b887e60d329087325c85
Ben30000 <47725693+Ben30000@users.noreply.github.com>
2020-02-09 23:11:19 -0600

    Commit #1: Updated MainActivity, added MainActivity2

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.lab2;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public void button1Clicked(View view) {
+        //Log.i( "Info", "Button Pressed");
+        EditText aTextField = (EditText) findViewById(R.id.textField1);
+        String s = aTextField.getText().toString();
+        //Toast.makeText(MainActivity.this,aTextField.getText().toString(),Toast.LENGTH_LONG).show();
+        goToActivity2(s);
+    }
+
+    public void goToActivity2(String s) {
+        Intent intent = new Intent(this,Main2Activity.class);
+        intent.putExtra("message",s);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
